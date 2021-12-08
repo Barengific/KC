12f8b66caa4a0f3328bb521a19fbd6371203abe3
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-08 18:03:03 +0000

    Update SecP256K1KeyGenerator.java
    Read from words_list file and find the seed phrase based on the private key.

--- src/main/java/com/kurdcoin/crypto/secp256k1/SecP256K1KeyGenerator.java
+++ src/main/java/com/kurdcoin/crypto/secp256k1/SecP256K1KeyGenerator.java
@@ -6,7 +6,12 @@ import org.bouncycastle.crypto.params.*;
 import org.bouncycastle.math.ec.ECPoint;
 import com.kurdcoin.crypto.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Implementation of the key generator for SECP256K1.
@@ -33,27 +38,72 @@ public class SecP256K1KeyGenerator implements KeyGenerator {
 		return new PublicKey(point.getEncoded(true));
 	}
 
-	/**
-	 * Test key generation, sign and verify.
-	 *
-	 * @param args The private key.
-	 * @return Nothing.
-	 */
-	public static void main(String[] args) { //throws NoSuchAlgorithmException {
-		SecP256K1KeyGenerator ss = new SecP256K1KeyGenerator();
-		KeyPair gg = ss.generateKeyPair();
-		PrivateKey privk = gg.getPrivateKey();
-		PublicKey pubk = ss.derivePublicKey(privk);
+	public static ArrayList<String> wordslist() throws FileNotFoundException {
+		ArrayList<String> result = new ArrayList<>();
 
-		String aa = "This is a simple message";
+		File filename = new File("src/main/resources/words_list.txt");
+		Scanner myReader = new Scanner(filename);
+		while (myReader.hasNextLine()) {
+			String data = myReader.nextLine();
+			result.add(data);
+		}
 
-		SecP256K1DsaSigner sign = new SecP256K1DsaSigner(gg);
+		return result;
+	}
 
-		Signature signature = sign.sign(aa.getBytes());
+	public static ArrayList<String> seed_phrase(String entropys) throws FileNotFoundException {
+		ArrayList<String> arr_entropy = new ArrayList<>();
+		ArrayList<Integer> decimalValue = new ArrayList<>();
+		ArrayList<String> wordslist = wordslist();
+		ArrayList<String> seed_words = new ArrayList<>();
 
-		boolean pp = sign.verify(aa.getBytes(), signature);
-		boolean pf = sign.verify("This is not the simple message".getBytes(), signature);
+		String entropy = convertStringToBinary(entropys);
+		int min = 0;
+		int max = 11;
+		for (int i = 0; i < 12; i++) {
+			arr_entropy.add(entropy.substring(min, max));
+			min += 11;
+			max += 11;
+			decimalValue.add(Integer.parseInt(arr_entropy.get(i), 2));
+			seed_words.add(wordslist.get(decimalValue.get(i)));
+		}
+		return seed_words;
+	}
 
+	public static String convertStringToBinary(String input) {
+
+		StringBuilder result = new StringBuilder();
+		char[] chars = input.toCharArray();
+		for (char aChar : chars) {
+			result.append(
+					String.format("%8s", Integer.toBinaryString(aChar))   // char -> int, auto-cast
+							.replaceAll(" ", "0")                         // zero pads
+			);
+		}
+		return result.toString();
 
 	}
+
+//	/**
+//	 * Test key generation, sign and verify.
+//	 *
+//	 * @param args The private key.
+//	 * @return Nothing.
+//	 */
+//	public static void main(String[] args) { //throws NoSuchAlgorithmException {
+//		SecP256K1KeyGenerator ss = new SecP256K1KeyGenerator();
+//		KeyPair gg = ss.generateKeyPair();
+//		PrivateKey privk = gg.getPrivateKey();
+//		PublicKey pubk = ss.derivePublicKey(privk);
+//
+//		String aa = "This is a simple message";
+//
+//		SecP256K1DsaSigner sign = new SecP256K1DsaSigner(gg);
+//
+//		Signature signature = sign.sign(aa.getBytes());
+//
+//		boolean pp = sign.verify(aa.getBytes(), signature);
+//		boolean pf = sign.verify("This is not the simple message".getBytes(), signature);
+//
+//	}
 }

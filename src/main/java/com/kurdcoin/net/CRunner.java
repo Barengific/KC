4e1476605b3a2b46b2bf85915cc02679b9c3faaf
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-09 15:50:53 +0000

    Update CRunner.java
    User keys static, visible to other classes. Finding ext IP on separate method.

--- src/main/java/com/kurdcoin/net/CRunner.java
+++ src/main/java/com/kurdcoin/net/CRunner.java
@@ -19,16 +19,24 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class CRunner extends Thread{
+public class CRunner extends Thread {
 
-    public String privk;
+    public static String privk;
+    public static String seed;
+    public static String pubK;
+    public static String wallet;
+
+    public static String Ip;
+    public static String msg;
+    ActionEvent event;
+
     public void here(String privk) throws IOException {
 
     }
 
     public CRunner() {
     }
-    ActionEvent event;
+
     public CRunner(ActionEvent event) {
         this.event = event;
     }
@@ -36,42 +44,46 @@ public class CRunner extends Thread{
     @Override
     public synchronized void run() {
         try {
-            URL whatismyip = new URL("http://checkip.amazonaws.com");
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    whatismyip.openStream()));
+            System.out.println("cRunner: " + msg);
+            if (msg.equals("1")) {
+                //System.out.println(SecP256K1KeyGenerator.seed_phrase(privk.toString()).toString());
+                ArrayList<String> seed = SecP256K1KeyGenerator.seed_phrase(privk.toString());
 
-            String ip = in.readLine(); //you get the IP as a String
-            System.out.println(ip);
-
-            System.out.println(SecP256K1KeyGenerator.seed_phrase(privk.toString()).toString());
-            ArrayList<String> seed = SecP256K1KeyGenerator.seed_phrase(privk.toString());
-//            KCController kc = new KCController();
-//            kc.seedPhrase(SecP256K1KeyGenerator.seed_phrase(privk.toString()));
-
-            Parent p = ((Node)event.getSource()).getScene().getRoot();
-            ObservableList<Node> aa = p.getChildrenUnmodifiable();
-            //System.out.println(aa.toString());
-            aa.get(3);
-            System.out.println(aa.get(3));
-            TextField tfSeedPhraseN = (TextField)aa.get(3);
-            tfSeedPhraseN.setText(seed.toString());
-
-//            tfSeedPhraseN.setText(seed.get(0));
-//            for(int i = 1; i < seed.size(); i++){
-//                tfSeedPhraseN.setText(tfSeedPhraseN.getText() + " " + seed.get(i));
+                Parent p = ((Node) event.getSource()).getScene().getRoot();
+                ObservableList<Node> aa = p.getChildrenUnmodifiable();
+//            System.out.println(aa.toString());
+//            for(int i = 0; i < aa.size(); i++){
+//                if(aa.get(i) == (Node)"TextField[id=tfSeedPhraseN, styleClass=text-input text-field]");
 //            }
-//            tfSeedPhraseN.setText("madeee itt");
-            //aa.set(3,(Node)tfSeedPhraseN);
+//            aa.get(3);
+                //System.out.println(aa.get(3));
+                TextField tfSeedPhraseN = (TextField) aa.get(4);
+                tfSeedPhraseN.setText(seed.toString());
 
-//
-//            FXMLLoader loader =  new FXMLLoader(getClass().getResource("/fxml/kc.fxml"));
-//            Parent calcRoot = loader.load();
-//            KCController controller = loader.getController();
-//            controller.tfSeedPhraseN.setText("made iittt");
-//            Scene showCalc = new Scene(calcRoot, 500, 1000);
-
+            } else if (msg.equals("2")) {
+                Parent p = ((Node) event.getSource()).getScene().getRoot();
+                ObservableList<Node> aa = p.getChildrenUnmodifiable();
+                System.out.println(aa.toString());
+            }
         } catch (Exception ex) {
             Logger.getLogger(CRunner.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public String readExIp() {
+        URL whatismyip = null;
+        try {
+            whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+
+            Ip = in.readLine(); //you get the IP as a String
+            System.out.println(Ip);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Ip;
     }
 }
